0dec0cb3992aad2cbb2a95d1465ccf6f98a8584f
bkfrye <brandonkfrye@gmail.com>
2022-05-04 10:29:23 -0400

    removed old js code that isn't needed

--- header.php
+++ header.php
@@ -35,36 +35,5 @@
     <title><?php wp_title(' | ', true, 'right')  . bloginfo('name'); ?></title>
   <?php  } ?>
 
-
   <?php wp_head(); ?>
-
-
-  <script type="text/javascript">
-    jQuery(document).ready(function($) {
-
-      if (window.location.href == "https://protectingourdignity.org/news/") {
-        $('#hero').remove();
-
-        $('.fl-post-grid-title').css("color", "white");
-        $('.fl-post-grid-more').css("color", "white");
-        $('.fl-post-grid-more').css("background-color", "#005A39");
-        $('.fl-post-grid-more').css("display", "inline-flex");
-        $('.fl-post-grid-more').css("align-items", "center");
-        $('.fl-post-grid-more').css("border-radius", "33px");
-        $('.fl-post-grid-more').css("padding", "9px 26px");
-        $('.fl-post-grid-more').append("&nbsp;");
-        $('.fl-post-grid-more').append("&nbsp;");
-        $('.fl-post-grid-more').append(" " + "<i class='fl-button-icon fl-button-icon-after ua-icon ua-icon-chevron-right-circle' aria-hidden='true'></i>");
-        $('.fl-post-grid-more').hover(function() {
-          $(this).css("background-color", "#F0562E");
-        }, function() {
-          $(this).css("background-color", "#005A39");
-        });
-
-
-      }
-    });
-  </script>
-
-
 </head>
